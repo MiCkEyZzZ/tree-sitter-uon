7aba9cb5d546edb5bd2da3be54f86558b38146b7
Mickeyzzz <zedmfix@gmail.com>
2026-07-09 15:09:06 +0300

    first commit

---
+++ grammar.js
@@ -0,0 +1,87 @@
+module.exports = grammar({
+  name: 'uon',
+
+  extras: $ => [
+    /\s/,
+    $.comment,
+  ],
+
+  conflicts: $ => [
+    [$.identifier, $.boolean],
+    [$.identifier, $.null],
+  ],
+
+  rules: {
+    document: $ => repeat($.value),
+
+    value: $ => choice(
+      $.object,
+      $.array,
+      $.string,
+      $.date,
+      $.number,
+      $.boolean,
+      $.null,
+      $.duration,
+      $.identifier,
+    ),
+
+    object: $ => seq(
+      '{',
+      optional(seq(
+        $.pair,
+        repeat(seq(',', $.pair)),
+        optional(','),
+      )),
+      '}',
+    ),
+
+    pair: $ => seq(
+      field('key', choice($.string, $.identifier)),
+      ':',
+      field('value', $.value),
+    ),
+
+    array: $ => seq(
+      '[',
+      optional(seq(
+        $.value,
+        repeat(seq(',', $.value)),
+        optional(','),
+      )),
+      ']',
+    ),
+
+    string: $ => seq(
+      '"',
+      repeat(choice(
+        /[^"\\]/,
+        /\\./,
+      )),
+      '"',
+    ),
+
+    number: $ => token(choice(
+      /\d+\.\d*([eE][+-]?\d+)?/,
+      /\d+/,
+    )),
+
+    boolean: $ => choice(
+      'истина',
+      'ложь',
+    ),
+
+    null: $ => 'пусто',
+
+    date: $ => token(/\d{4}-\d{2}-\d{2}/),
+
+    duration: $ => token(/\d+(?:[гдмсч]|мин)(?:\d+(?:[гдмсч]|мин))*/),
+
+    identifier: $ => /[_\p{L}][_\p{L}\p{N}]*/,
+
+    comment: $ => token(choice(
+      /\/\/[^\n]*/,
+      /\/\*[\s\S]*?\*\//,
+    )),
+  },
+});
